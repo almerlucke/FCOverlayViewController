ca1e16e1a4155e89a29f93fe23923c185750468a
Almer Lucke <almer.lucke@gmail.com>
2013-11-10 14:51:16 +0100

    Added comments

--- FCOverlayViewController/FCOverlayViewController.h
+++ FCOverlayViewController/FCOverlayViewController.h
@@ -7,11 +7,26 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  Use the presentOverlayWithViewController class method to present a view controller in a new window
+ *  on top of everything else. The window level is set to normal so the status bar is not covered.
+ *
+ *  The overlay can be dismissed from the presented view controller with 
+ *  [self.presentingViewController dismissViewControllerAnimated:animated completion:completion] OR with a call to
+ *  [FCOverlayViewController dismissOverlayAnimated:animated completion:completion]. 
+ *
+ *  The two class methods to dismiss the overlay(s) are convenience methods so the overlay(s) can be dismissed from
+ *  any part of the application.
+ *
+ *  All auto rotation methods are forwarded to the presented view controller to allow the presented view controller
+ *  to decide which rotations are allowed
+ */
+
 @interface FCOverlayViewController : UIViewController
 
 /**
  *  Present a view controller in a new window, use FCOverlayViewController as in between root view controller
- *  to be able to present the given view controller in an animated way
+ *  to be able to present the given view controller in an animated way.
  *
  *  @param controller
  *  @param animated
@@ -23,7 +38,7 @@
 
 /**
  *  Dismiss topmost overlay view controller plus it's window, this can also be accomplished by
- *  calling dismissViewControllerAnimated on the presentingViewController of the presented overlayed view controller
+ *  calling dismissViewControllerAnimated on the presentingViewController of the presented overlayed view controller.
  *
  *  @param animated
  *  @param completion
